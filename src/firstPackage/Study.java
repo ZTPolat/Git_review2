8452121a64a534cdb0e4eecd15ba59efeed00cdb
ztpolat <ztpolat12@gmail.com>
2023-01-15 23:15:38 -0500

    changed files

---
+++ src/firstPackage/Study.java
@@ -0,0 +1,11 @@
+package firstPackage;
+
+public class Study {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hi");
+
+        System.out.println("My name is Rose!");
+    }
+}
